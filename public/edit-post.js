1cd4a5c3b334406075e0996724da1028e4eafd17
chrisnastro <cnastro@icloud.com>
2024-03-11 16:50:20 -0400

    added edit post js

---
+++ public/edit-post.js
@@ -0,0 +1,49 @@
+const post_is = window.location.toString().split('/')[
+    window.location.toString().split('/').length - 1
+];
+
+const updatePost = async (event) => {
+    event.preventDefault();
+    const title = document.querySelector('#title-update-post').value.trim();
+    const content = document.querySelector('#content-update-post').value.trim();
+
+    if (title && content) {
+        const response = await fetch(`/api/post/${post_id}`, {
+            method: 'PUT',
+            body: JSON.stringify({ title, content }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert('Unable to update');
+        }
+    }
+};
+
+const deletePost = async (event) => {
+    event.preventDefault();
+
+    const response = await fetch(`/api/posts/${post_id}`, {
+        method: 'DELETE',
+    });
+
+    if (response.ok) {
+        document.location.replace('/dashboard');
+    } else {
+        alert('Unable to delete post');
+    }
+};
+
+const updatePostButton = document.querySelector('#update-post');
+
+if (updatePostButton) {
+    updatePostButton.addEventListener('click', updatePost);
+}
+
+const deletePostButton = document.querySelector('#delete-post');
+
+if (deletePostButton) {
+    deletePostButton.addEventListener('click', deletePost);
+}
